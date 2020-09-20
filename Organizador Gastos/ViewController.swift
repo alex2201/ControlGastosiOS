fdce925a922cfb539e243fc139b4a401c8138469
Alexander López <alcedillo@grupoautofin.com>
2020-09-19 22:11:48 -0500

    Initial Commit

---
+++ Organizador Gastos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Organizador Gastos
+//
+//  Created by Alexander Lopez on 19/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
