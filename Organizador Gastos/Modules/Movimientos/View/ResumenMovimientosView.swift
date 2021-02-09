1f24df90075c958b451a62b7bff98cd16acb1e6a
Alexander Lopez Cedillo <alcedillo@grupoautofin.com>
2021-02-09 15:25:05 -0600

    Agregada tabla de lista de movimientos

--- Organizador Gastos/Modules/Movimientos/View/ResumenMovimientosView.swift
+++ Organizador Gastos/Modules/Movimientos/View/ResumenMovimientosView.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ResumenMovimientosView: UIView {
     
+    // MARK: Variables públicas
     let montoTotalLbl = UILabel()
+    let tablaMovimientos = UITableView(frame: .zero, style: .insetGrouped)
     
+    // MARK: Variables privadas
     private let montoFormatter = StringFormatter.moneyFormatter
     
     override init(frame: CGRect) {
@@ -21,20 +24,44 @@ class ResumenMovimientosView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
+    // MARK: Configuración UI
     private func configurarUI() {
         backgroundColor = .white
+        layoutMargins = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
         
         addSubview(montoTotalLbl)
+        addSubview(tablaMovimientos)
+        
+        configurarMontoLbl()
+        configurarTablaMovimientos()
+    }
+    
+    private func configurarMontoLbl() {
+        montoTotalLbl.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
         montoTotalLbl.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            montoTotalLbl.centerYAnchor.constraint(equalTo: centerYAnchor),
             montoTotalLbl.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            montoTotalLbl.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
         ])
     }
     
+    private func configurarTablaMovimientos() {
+        tablaMovimientos.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tablaMovimientos.topAnchor.constraint(equalTo: montoTotalLbl.bottomAnchor, constant: 24),
+            tablaMovimientos.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tablaMovimientos.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tablaMovimientos.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    // MARK: Métodos públicos
     func configurar(montoTotal: Double) {
-        montoTotalLbl.text = montoFormatter.string(from: NSNumber(value: montoTotal))
+        let montoString = montoFormatter.string(from: NSNumber(value: montoTotal))
+        montoTotalLbl.text = "\(montoString ?? "N/A")"
     }
 }
