fb3e56d65af81c04207c4923142189901ba80b59
Alexander Lopez Cedillo <alcedillo@grupoautofin.com>
2021-02-09 17:54:10 -0600

    actualización estilo barra navegacion

--- Organizador Gastos/Modules/Movimientos/Controller/MovimientosNC.swift
+++ Organizador Gastos/Modules/Movimientos/Controller/MovimientosNC.swift
@@ -11,6 +11,19 @@ class MovimientosNC: UINavigationController {
     
     weak var coordinator: MovimientosCoordinator?
     
+    override init(nibName nibNameOrNil: String? = nil, bundle nibBundleOrNil: Bundle? = nil) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor = .cyan
+    }
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         coordinator?.finish()
