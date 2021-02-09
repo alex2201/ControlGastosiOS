e69d74541237c457940d18fbcd0c55f024b52e84
Alexander Lopez Cedillo <alcedillo@grupoautofin.com>
2021-02-09 15:21:00 -0600

    Añadido set de movimientos de prueba

--- Organizador Gastos/Modules/Movimientos/Model/Movimiento.swift
+++ Organizador Gastos/Modules/Movimientos/Model/Movimiento.swift
@@ -11,4 +11,29 @@ struct Movimiento {
     let descripcion: String
     let monto: Double
     let fecha: Date
+    
+    static func dummySet() -> [Movimiento] {
+        return [
+            Movimiento(
+                descripcion: "Descripción del movimiento 1",
+                monto: 523.25,
+                fecha: Date.fromString("23/01/2021", usingFormat: "dd/mm/yyyy")!
+            ),
+            Movimiento(
+                descripcion: "Descripción del movimiento 2",
+                monto: 523.25,
+                fecha: Date.fromString("19/01/2021", usingFormat: "dd/mm/yyyy")!
+            ),
+            Movimiento(
+                descripcion: "Descripción del movimiento 1",
+                monto: 523.25,
+                fecha: Date.fromString("19/01/2021", usingFormat: "dd/mm/yyyy")!
+            ),
+            Movimiento(
+                descripcion: "Descripción del movimiento 1",
+                monto: 523.25,
+                fecha: Date.fromString("12/02/2021", usingFormat: "dd/mm/yyyy")!
+            ),
+        ]
+    }
 }
